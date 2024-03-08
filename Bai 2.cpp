58f3e0382720afd0498ad941b7c3af3846257628
Tran Dang Quang <vanlytuongthanh1997@gmail.com>
2024-03-08 17:22:24 +0700

    add

---
+++ Bai 2.cpp
@@ -0,0 +1,14 @@
+/* Bai 2 : Bien va kieu du lieu */
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n=3;
+	printf("Ket qua : n = %d\n",n);
+	float m=0.125;
+	printf("Ket qua : m = %.2f\n",m);
+}
+ 
